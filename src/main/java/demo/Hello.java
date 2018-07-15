b7e8895776623926224cdcec321fe27e4bad14d8
freewind <nowindlee@gmail.com>
2018-07-15 17:38:30 +0800

    java8

--- src/main/java/demo/Hello.java
+++ src/main/java/demo/Hello.java
@@ -1,10 +1,12 @@
 package demo;
 
 import javafx.application.Application;
-import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
-import javafx.scene.layout.*;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.Priority;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class Hello extends Application {
